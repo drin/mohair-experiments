c07c8e99c93959d962f1cd1ff9e5396554e24063
Aldrin Montana <octalene.dev@pm.me>
2024-02-23 12:50:39 -0800

    re-adding test-query

---
+++ resources/scripts/duckdb-sql/test-query.sql
@@ -0,0 +1,18 @@
+  SELECT  gene_id
+         ,COUNT(*)        AS cell_count
+         ,AVG(e.expr)     AS expr_avg
+         ,VAR_POP(e.expr) AS expr_var
+    FROM metaclusters mc
+
+          JOIN clusters c
+         USING (cluster_id)
+
+          JOIN cluster_membership cm
+         USING (cluster_id)
+
+          JOIN  expression e
+         USING (cell_id)
+
+   WHERE mc.mcluster_id = 12
+
+GROUP BY e.gene_id
