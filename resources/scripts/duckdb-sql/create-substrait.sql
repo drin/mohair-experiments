86bf3ffa99c583ed10228aa73e04fd2a6fe23742
Aldrin Montana <octalene.dev@pm.me>
2024-03-22 20:32:35 -0700

    task: updated sample query scripts

--- resources/scripts/duckdb-sql/create-substrait.sql
+++ resources/scripts/duckdb-sql/create-substrait.sql
@@ -15,22 +15,24 @@ LOAD    substrait;
 -- Define Query
 
 CALL get_substrait('
-  SELECT  gene_id
-         ,COUNT(*)        AS cell_count
-         ,AVG(e.expr)     AS expr_avg
-         ,VAR_POP(e.expr) AS expr_var
-    FROM metaclusters mc
-
-          JOIN clusters c
-         USING (cluster_id)
-
-          JOIN cluster_membership cm
-         USING (cluster_id)
-
-          JOIN  expression e
-         USING (cell_id)
-
-   WHERE mc.mcluster_id = 12
-
-GROUP BY e.gene_id
+  EXPLAIN(
+    SELECT  gene_id
+           ,COUNT(*)        AS cell_count
+           ,AVG(e.expr)     AS expr_avg
+           ,VAR_POP(e.expr) AS expr_var
+      FROM metaclusters mc
+  
+            JOIN clusters c
+           USING (cluster_id)
+  
+            JOIN cluster_membership cm
+           USING (cluster_id)
+  
+            JOIN  expression e
+           USING (cell_id)
+  
+     WHERE mc.mcluster_id = 12
+  
+  GROUP BY e.gene_id
+  )
 ');
